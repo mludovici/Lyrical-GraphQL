e71066c7008722c2fa3f48f41a41b767ea59d0bb
Marc Ludovici <marc.ludovici@netmedia.de>
2020-01-15 17:40:51 +0100

    list verbessern

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,12 +3,24 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li key={song.id} className="collection-item">
+                    {song.title}
+                </li>
+            )
+        })
+    }
+
     render() {
-        console.log(this.props);
+        if (this.props.data.loading) { return <div>Loading...</div>}
+
         return (
-            <div>
-                SongList
-            </div>
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
+              
         )
     }
 }
@@ -16,7 +28,8 @@ class SongList extends Component {
 const query = gql`
     {
         songs {
-            title
+            title,
+            id
         }
     }
 `;
