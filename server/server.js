5a8fbe6777785d27730a85722f461674e8368e4c
Marc Ludovici <marc.ludovici@netmedia.de>
2020-01-15 17:17:45 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -8,13 +8,27 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+//const MONGO_URI = 'mongodb://MALMAL:IsgiInet123!@ds221631.mlab.com:21631/dnz';
+//const MONGO_URI = 'mongodb://localhost:27017/test';
+const MONGO_URI = 'mongodb://stephen:stephen@ds021182.mlab.com:21182/lyricaldb';
+
+
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
 
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://marc:marc@cluster0-wjof5.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {useMongoClient: true});
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
     .on('error', error => console.log('Error connecting to MongoLab:', error));
